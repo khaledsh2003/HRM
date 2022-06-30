076786723bb67388d407c9d973cb4ec3500980cd
khaled shaheen <64599011+khaledsh2003@users.noreply.github.com>
2022-06-30 09:54:14 +0300

    added  nvarchar(50) for appropriate fields

--- HRM.DAL/UserEntity.cs
+++ HRM.DAL/UserEntity.cs
@@ -12,11 +12,16 @@ namespace HRM.DAL
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
-        public Guid UserID {get;set;} 
+        public Guid UserID {get;set;}
+        [MaxLength(50)]
         public string Name { get;set;}
-        public string MobileNumber { get;set;}
+        [MaxLength(70)]
+        public string MobileNumber { get; set; }
+        [MaxLength(100)]
         public string Email { get;set;}
+        [MaxLength(100)]
         public string Password { get; set; }
+        [MaxLength(70)]
         public string JobTitle { get; set; }
         public Guid? ManagerID { get; set; }
 
