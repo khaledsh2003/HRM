a069b5d85f94f0222b5d2622a1a0a5dddc43817a
khaled shaheen <64599011+khaledsh2003@users.noreply.github.com>
2022-07-05 10:18:22 +0300

    added vacation enums

---
+++ HRM.Models/VacationEnums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public enum VacationTypes
+    {
+        Annual=0,
+        Sick=1,
+        Leave=2,
+        Exceptional=3
+    }
+    public enum VacationStatus
+    {
+         Draft=0,
+         Submitted=1,
+         Approved=2,
+         Rejected=3
+    }
+}
